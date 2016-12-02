af0a10159ebc03303219b5c9c4c800ac049cbee1
Rcorvo <rcu1987@gmail.com>
2016-12-02 23:44:32 +0100

    Actividad 5.9 terminada

--- app/src/main/java/com/example/pantallatactil/PantallaTactilActivity.java
+++ app/src/main/java/com/example/pantallatactil/PantallaTactilActivity.java
@@ -20,7 +20,20 @@ public class PantallaTactilActivity extends AppCompatActivity implements View.On
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         TextView salida = (TextView) findViewById(R.id.txtPulsaciones);
-        salida.append("\n" + motionEvent.toString() + "\n");
+        //salida.append("\n" + motionEvent.toString() + "\n");
+        String acciones[] = { "ACTION_DOWN", "ACTION_UP", "ACTION_MOVE", "ACTION_CANCEL",
+                "ACTION_OUTSIDE", "ACTION_POINTER_DOWN", "ACTION_POINTER_UP" };
+        int action = motionEvent.getAction();
+        int codigoAccion = action & MotionEvent.ACTION_MASK;
+        int iPuntero = (action & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        salida.append("\n" + acciones[codigoAccion]);
+        salida.append(" Generada por puntero: " + iPuntero);
+        for(int i = 0; i < motionEvent.getPointerCount(); i++){
+            salida.append("\npuntero: " + motionEvent.getPointerId(i) +
+            " x:" + motionEvent.getX(i) + " y:" + motionEvent.getY(i));
+        }
+        salida.append("\n");
 
         return true;
     }
